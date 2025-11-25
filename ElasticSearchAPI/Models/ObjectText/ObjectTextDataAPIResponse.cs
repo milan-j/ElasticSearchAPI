097ba3c60df6d6486e86f8b35b13a673c8b67b3f
Milan Janackovic <milanjanackovic@gmail.com>
2025-11-25 22:39:30 +0100

    refactoring

--- ElasticSearchAPI/Models/ObjectText/ObjectTextDataAPIResponse.cs
+++ ElasticSearchAPI/Models/ObjectText/ObjectTextDataAPIResponse.cs
@@ -1,9 +1,18 @@
 ﻿namespace ElasticSearchAPI.Models.ObjectText
 {
+    /// <summary>
+    /// Represents the response returned from an API call that retrieves a collection of ObjectTextData.
+    /// </summary>
     public class ObjectTextDataAPIResponse
     {
+        /// <summary>
+        /// Gets or sets the collection of object text data.
+        /// </summary>
         public required IEnumerable<ObjectTextData> Objects { get; set; }
 
+        /// <summary>
+        /// Gets the number of elements contained in the Objects collection.
+        /// </summary>
         public int Count => Objects.Count();
     }
 }
